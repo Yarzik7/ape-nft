531001e60ffaaec2dd4f2c254683831787f16e57
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-03-21 19:37:16 +0200

    Work with tablet and desktop version of Hero
    - Added and refactoring styles
    - Added image box

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import Section from '../Section/Section';
 import Image from 'next/image';
 import Button from '../Button/Button';
 import Text from '../Text/Text';
+import { classNameJoin } from '@/utils';
 import styles from './Hero.module.css';
 
 import heroData from '../../data/hero.json';
@@ -16,16 +17,25 @@ const Hero = () => {
       <div className={styles.heroTitleBox}>
         <p className={styles.heroSlogan}>{slogan[0]}</p>
         <h1 className={styles.heroTitle}>{title}</h1>
-        <p className={styles.heroSlogan}>{slogan[1]}</p>
+        <p
+          className={classNameJoin(
+            styles.heroSlogan,
+            styles.heroSloganWithPadding
+          )}
+        >
+          {slogan[1]}
+        </p>
       </div>
 
-      {/* <Image
-        src={src.mobile}
-        alt="chimpanzee"
-        width={216}
-        height={284}
-        className={styles.heroImage}
-      /> */}
+      <div className={styles.heroImageBox}>
+        <Image
+          src={src.mobile}
+          alt="chimpanzee"
+          width={216}
+          height={284}
+          className={styles.heroImage}
+        />
+      </div>
 
       <div className={styles.heroDescriptionBox}>
         <Button content={buttonCaption} className={styles.meetApesButton} />
