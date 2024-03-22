89a22340d1542143ea17c33db71c46f98f665931
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-03-22 13:56:51 +0200

    work with FAQCard component

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,17 +1,35 @@
+'use client';
+
 import Section from '../Section/Section';
 import Title from '../Title/Title';
 import FAQCard from './FAQCard/FAQCard';
+import { useState } from 'react';
 import styles from './FAQ.module.css';
 
 import FAQData from '../../data/FAQ.json';
 const { title, questions } = FAQData;
 
 const FAQ = () => {
+  const [activeQuestion, setActiveQuestion] = useState(0);
+
   return (
     <Section>
       <Title title={title} />
       <ul>
-        <FAQCard key="0" questionNumber={1} info={questions[0]} />
+        <FAQCard
+          key="0"
+          questionNumber={0}
+          activeQuestion={activeQuestion}
+          setActiveQuestion={setActiveQuestion}
+          info={questions[0]}
+        />
+        <FAQCard
+          key="1"
+          questionNumber={1}
+          activeQuestion={activeQuestion}
+          setActiveQuestion={setActiveQuestion}
+          info={questions[1]}
+        />
       </ul>
     </Section>
   );
--- src/components/FAQ/FAQCard/FAQCard.jsx
+++ src/components/FAQ/FAQCard/FAQCard.jsx
@@ -1,13 +1,17 @@
 import Text from '@/components/Text/Text';
 import styles from './FAQCard.module.css';
 
-const FAQCard = ({ questionNumber, info: { question, answer } }) => {
+const FAQCard = ({ questionNumber, activeQuestion, setActiveQuestion, info: { question, answer } }) => {
+  const isActive = questionNumber === activeQuestion;
+
+  const onSetActiveQuestion = () => setActiveQuestion(questionNumber - 1);
+
   return (
-    <li className={styles.FAQCardItem}>
-      <p className={styles.questionNumber}>{`[${questionNumber}]`}</p>
+    <li className={styles.FAQCardItem} tabIndex={0} onClick={onSetActiveQuestion}>
+      <p className={styles.questionNumber}>{`[${questionNumber + 1}]`}</p>
       <div>
         <h3 className={styles.FAQCardQuestion}>{question}</h3>
-        <Text text={answer} className={styles.FAQCardAnswer} />
+        {isActive && <Text text={answer} className={styles.FAQCardAnswer} />}
       </div>
     </li>
   );
