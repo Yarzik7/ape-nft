cac655052da39c34c1e2437058b25954a1ceed82
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-03-29 20:44:47 +0200

    Update NavLink.jsx

--- src/components/NavLink/NavLink.jsx
+++ src/components/NavLink/NavLink.jsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import styles from './NavLink.module.css';
 
-const NavLink = ({ href, buttonAsLink = false, className = '', children, isSocial = false, onClick }) => {
+const NavLink = ({ href, buttonAsLink = false, className = '', children, onClick }) => {
   return !buttonAsLink ? (
     <Link href={href} target="_blank" rel="noopener noreferrer nofollow" className={styles.navLink + ' ' + className}>
       {children}
