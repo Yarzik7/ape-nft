041c3f718995fbbd5e8400a1b3b2269e9946fdba
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-03-30 21:38:26 +0200

    work with react hook form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,28 @@
+'use client';
+
 import Section from '../Section/Section';
 import Title from '../Title/Title';
 import Text from '../Text/Text';
 import Form from '../Form/Form';
 import Input from '../Form/Input/Input';
+import { useForm } from 'react-hook-form';
 import styles from './Contact.module.css';
 
 import contactData from '../../data/contact.json';
 const { title, invitationText } = contactData;
 
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = () => {
+    console.log('onSubmit');
+  };
+
   return (
     <Section id="Mint">
       <Title title={title} className={styles.contactTitle} />
@@ -17,7 +31,7 @@ const Contact = () => {
           <Text text={invitationText} withCrossIcon crossIconClasses={styles.crossIconInContactBlock} />
         </div>
 
-        <Form buttonCaption="Mint">
+        <Form buttonCaption="Mint" onSubmit={handleSubmit(onSubmit)}>
           <Input label="discord" placeholder="@username" labelClasses={styles.discordLabel} />
           <Input label="metamask" placeholder="Wallet address" />
         </Form>
