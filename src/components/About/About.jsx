1e551d50110ebadacb37659e3225d3c743434112
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-04-04 01:45:59 +0300

    using clsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,6 +3,7 @@ import Container from '../Section/Container/Container';
 import Text from '../Text/Text';
 import Image from 'next/image';
 import CreepingLine from './CreepingLine/CreepingLine';
+import { clsx } from 'clsx';
 import styles from './About.module.css';
 
 import aboutData from '../../data/about.json';
@@ -16,7 +17,7 @@ const About = () => {
           <h2 className={styles.aboutTitle}>
             {title[0]}
             <span className={styles.aboutTitleAccent}>{title[1]}</span>
-            <span className={styles.aboutTitleAccent + ' ' + styles.aboutTitleApe}>{title[2]}</span>
+            <span className={clsx(styles.aboutTitleAccent, styles.aboutTitleApe)}>{title[2]}</span>
           </h2>
           <Text text={text[0]} className={styles.aboutTextInTitleBlock} />
         </div>
@@ -32,21 +33,21 @@ const About = () => {
               alt="ape"
               width={216}
               height={292}
-              className={styles.aboutImage + ' ' + styles.mobile}
+              className={clsx(styles.aboutImage, styles.mobile)}
             />
             <Image
               src={src.tablet}
               alt="ape"
               width={313}
               height={422}
-              className={styles.aboutImage + ' ' + styles.tablet}
+              className={clsx(styles.aboutImage, styles.tablet)}
             />
             <Image
               src={src.desktop}
               alt="ape"
               width={492}
               height={662}
-              className={styles.aboutImage + ' ' + styles.desktop}
+              className={clsx(styles.aboutImage, styles.desktop)}
             />
           </div>
         </div>
--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -1,11 +1,12 @@
 import CrossIcon from '../CrossIcon/CrossIcon';
+import { clsx } from 'clsx';
 import styles from './Text.module.css';
 
 const Text = ({ text, className = '', withCrossIcon = false, crossIconClasses = '' }) => {
   return (
     <>
-      {withCrossIcon && <CrossIcon className={`${styles.crossIconInTextBlock + ' ' + crossIconClasses}`} />}
-      <p className={styles.text + ' ' + className}>{text}</p>
+      {withCrossIcon && <CrossIcon className={clsx(styles.crossIconInTextBlock, crossIconClasses)} />}
+      <p className={clsx(styles.text, className)}>{text}</p>
     </>
   );
 };
